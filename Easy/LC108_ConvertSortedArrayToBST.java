097d2da1ab512f8f8aa2513609debaa3b1a3d912
Abujar Shaikh <103555266+abujarshaikh@users.noreply.github.com>
2026-02-03 20:22:38 +0530

    Solve LC 108: Convert Sorted Array to Height-Balanced BST

--- Easy/LC108_ConvertSortedArrayToBST.java
+++ Easy/LC108_ConvertSortedArrayToBST.java
@@ -23,22 +23,6 @@
 // Due to recursion stack for a balanced tree.
 // (Worst case: O(n) recursion depth, but input guarantees balance.)
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
-
 class Solution {
      public TreeNode sortedArrayToBST(int[] nums) {
           return build(nums, 0, nums.length - 1);
